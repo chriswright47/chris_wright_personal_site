47ef952fddfacc016d2027cd80ff6eefe57c04b8
Chris Wright <christopher.wright47@gmail.com>
2013-12-07 15:43:56 -0600

    removed username and password from db file

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ChrisWrightSite::Application.config.secret_token = '2c676374c46ab9dff74907994d3035b8b4a0c0373849bbca58cdb2f246385d6b3e4ce58db8d28cb5a6b5262c8868214d1ef026d16722907ee01e13b145787ffb'
